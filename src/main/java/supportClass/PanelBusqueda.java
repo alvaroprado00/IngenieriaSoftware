1c5f4361d51df092db2fd90b8f71878b72bf5558
Lawrence4U <60020067+Lawrence4U@users.noreply.github.com>
2020-10-09 18:56:56 +0200

    Avance con los paneles que se muestran en la busqueda

--- src/main/java/supportClass/PanelBusqueda.java
+++ src/main/java/supportClass/PanelBusqueda.java
@@ -1,7 +1,22 @@
 package supportClass;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class PanelBusqueda extends JPanel {
+
+    public PanelBusqueda(String nombre,javax.swing.ImageIcon foto){
+
+        this.setLayout(new BorderLayout());
+
+        JLabel imagen = new JLabel(foto);
+        JLabel titulo = new JLabel(nombre);
+
+        this.add(imagen, BorderLayout.NORTH);
+        this.add(titulo, BorderLayout.SOUTH);
+        
+
+    }
 
 }
