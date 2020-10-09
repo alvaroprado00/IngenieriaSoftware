7efe5587de2d8111e29265c83f68d76d670f6896
Nachols7 <72037934+Nachols7@users.noreply.github.com>
2020-10-09 11:03:43 +0200

    Clase Pala creada
    Con constructor, getters y setters

---
+++ src/main/java/model/Pala.java
@@ -0,0 +1,93 @@
+package model;
+/**
+ * Clase creada para clasificar a las palas de la aplicación
+ *
+ * @author Ignacio Lopez
+ */
+public class Pala {
+
+    private String nombre; //Todos los atributos que tiene una pala
+    private String forma;
+    private int potencia;
+    private int control;
+    private int salida;
+    private int manejabilidad;
+    private int puntoDulce;
+    private int total;
+    private float precio;
+
+    public Pala(String nombre,String forma,int potencia,int control,int salida, int manejabilidad,int puntoDulce,float precio){
+        this.nombre=nombre;
+        this.forma=forma;
+        setPotencia(potencia);
+        setControl(control);
+        setSalida(salida);
+        setManejabilidad(manejabilidad);
+        setPuntoDulce(puntoDulce);
+        if(precio > 0)
+            this.precio=precio;
+        this.total = (potencia + control + salida + manejabilidad + puntoDulce)/5;
+    }
+
+    public void setPotencia(int potencia){
+        if(potencia >= 0 && potencia <= 100)
+            this.potencia=potencia;
+    }
+
+    public void setControl(int control){
+        if(control >= 0 && control <= 100)
+            this.control=control;
+    }
+
+    public void setSalida(int salida){
+        if(salida >= 0 && salida <= 100)
+            this.salida=salida;
+    }
+
+    public void setManejabilidad(int manejabilidad){
+        if(manejabilidad >= 0 && manejabilidad <= 100)
+            this.manejabilidad=manejabilidad;
+    }
+
+    public void setPuntoDulce(int puntoDulce){
+        if(puntoDulce >= 0 && puntoDulce <= 100)
+            this.puntoDulce=puntoDulce;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public int getControl() {
+        return control;
+    }
+
+    public int getSalida() {
+        return salida;
+    }
+
+    public int getManejabilidad() {
+        return manejabilidad;
+    }
+
+    public int getPuntoDulce() {
+        return puntoDulce;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+}
