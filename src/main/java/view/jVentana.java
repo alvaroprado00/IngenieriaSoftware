14a38b34a87fd9adc3848f08260f2d14e9957cbc
Lawrence4U <60020067+Lawrence4U@users.noreply.github.com>
2020-10-10 17:35:30 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/alvaroprado00/IngenieriaSoftware""
    This reverts commit de50aa9c3a5365c2a12dad4e8db7a6a7525d8e26.

---
+++ src/main/java/view/jVentana.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.*;
+
+public class jVentana {
+    public static void main(String argv[])
+    {
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel pnl = new Headline();
+        jFrame.add(pnl);
+        jFrame.setTitle("Hola");
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+
+}
